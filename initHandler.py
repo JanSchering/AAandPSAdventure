ad88676e69e140413b44ffa1a7a7e1e63ca9dcd2
TaraCarette <terra.the.great@gmail.com>
2021-12-13 15:51:36 +0100

    Add comments, clean up unused/unneeded things

--- initHandler.py
+++ initHandler.py
@@ -1,24 +1,31 @@
 from __future__ import print_function, unicode_literals
 import json
-from os import path, getcwd, listdir, remove, mkdir
+from os import path, getcwd, listdir, mkdir
 from whaaaaat import prompt
 from util import style, init_state, Keys, state_path
-from datetime import datetime
 
 
 def get_init_state():
+    # if does not exist, create a directory to hold the saved states
     if not path.exists(state_path):
         mkdir(state_path)
 
+    # get and sort the pre-existing save state files
     saveState_files = listdir(path.join(getcwd(), "saveStates"))
     saveState_files.sort()
-    timestamp = datetime.now().strftime("%d-%m-%Y-%H-%M-%S")
+
+    # if saved files exist offer the user to choice to load from a previous state
     if len(saveState_files) > 0:
+        # load each existing save state
         saveStates = [json.load(open(path.join(state_path, file_path)))
                       for file_path in saveState_files]
+
+        # extract chapter name, checkpoint numner, and username for each saved state
         content = [str({Keys.CHAP.value: saveState[Keys.CHAP.value],
                         Keys.CHECKP.value: saveState[Keys.CHECKP.value],
                         Keys.NAME.value: saveState[Keys.CHR_NAME.value]}) for saveState in saveStates]
+
+        # prompt user to to choose an old saved state or start new game
         prompt_name = "loadstate_prompt"
         opt_new_game = "new game"
         load_prompt = {
@@ -27,13 +34,16 @@ def get_init_state():
             "message": "Choose the checkpoint to load",
             "choices": [*content, opt_new_game],
         }
+        # get answer from user and fills in prompt_name with the answer
         ans = prompt(load_prompt, style=style)
+
+        # start the game
         if ans[prompt_name] == opt_new_game:
             state = init_state
         else:
+            # get index of chosen save state
             an_idx = load_prompt[Keys.CHOICES.value].index(ans[prompt_name])
-            with open(path.join(state_path, saveState_files[an_idx]), "r") as f:
-                state = json.load(f)
+            state = saveStates[an_idx]
     else:
         state = init_state
 
